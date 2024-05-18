726904b63bc7aa11e2aba1124ff7207cd2120216
Justus von der Beek <justusvonderbeek@gmail.com>
2024-05-18 12:39:44 +0200

    add if down, path loss tests

--- mininet/testing.py
+++ mininet/testing.py
@@ -40,7 +40,48 @@ def quicheperf(net, directory, conf):
 
     # print("Executing: {}".format(f"{quicheperf_dir}/target/{target}/quicheperf server --cert {quicheperf_dir}/src/cert.crt --key {quicheperf_dir}/src/cert.key -l 192.168.1.3:10000 --mp true"))
 
+    if conf.log_level.value > Logging.INFO.value:
+        server = h2.popen(f"{quicheperf_dir}/target/{target}/quicheperf server --cert {quicheperf_dir}/src/cert.crt --key {quicheperf_dir}/src/cert.key -l 192.168.1.3:10000 --mp true &> {testing_dir}/{directory}/h2.log", shell=True)
+
+        client = h1.popen(f"{quicheperf_dir}/target/{target}/quicheperf client -l 192.168.1.2:20000 -c 192.168.1.3:10000 --mp true -d {conf.duration} -b {tp} &> {testing_dir}/{directory}/h1.log", shell=True)
+
+        server_capture = (server, None)
+        client_capture = (client, None)
+        output_processes.append(server_capture)
+        output_processes.append(client_capture)
+    else:
+        server = h2.popen(f"{quicheperf_dir}/target/{target}/quicheperf server --cert {quicheperf_dir}/src/cert.crt --key {quicheperf_dir}/src/cert.key -l 192.168.1.3:10000 --mp true", stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+        client = h1.popen(f"{quicheperf_dir}/target/{target}/quicheperf client -l 192.168.1.2:20000 -c 192.168.1.3:10000 --mp true -d {conf.duration} -b {tp}", stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    
+        server_capture = (server, f"{directory}/h2.log")
+        client_capture = (client, f"{directory}/h1.log")
+        output_processes.append(server_capture)
+        output_processes.append(client_capture)
+
+    # path_loss(net, "h1", "h1-wifi")
+    wait(15)
+    # path_loss(net, "h1", "h1-wifi", loss=0)
+    # wait()
+
+    # Finished testing
+    return output_processes
+
+def quicheperf_if_test(net, directory, conf):
+    """
+    Testing the behavior of the implementation in case an interface goes up or down.
+    """
+
+    h1 = net.get("h1")
+    h2 = net.get("h2")
+
+    target = conf.build_target
+    tp = conf.throughput
+    output_processes = []
+
     ip_storage = iface_down(net, "h1", "h1-eth", directory)
+    # Ensure interface is down
+    wait(0.5)
 
     if conf.log_level.value > Logging.INFO.value:
         server = h2.popen(f"{quicheperf_dir}/target/{target}/quicheperf server --cert {quicheperf_dir}/src/cert.crt --key {quicheperf_dir}/src/cert.key -l 192.168.1.3:10000 --mp true &> {testing_dir}/{directory}/h2.log", shell=True)
@@ -61,16 +102,58 @@ def quicheperf(net, directory, conf):
         output_processes.append(server_capture)
         output_processes.append(client_capture)
 
-    # Configure waiting etc. here
-
-    wait()
-    # path_loss(net, "h1", "h1-wifi")
+    # Waiting long enough so that we don't match the re-gathering exactly
+    wait(12)
+    # Restore interface and IP, waiting for ICE to notice
     ip_storage = iface_up(net, "h1", "h1-eth", directory, ip_storage)
-    wait(10)
-    # path_loss(net, "h1", "h1-wifi", loss=0)
-    # wait()
-    # Setting the interface down, etc.
+    wait(15)
 
     # Finished testing
+    return output_processes
 
+def quicheperf_path_loss_test(net, directory, conf):
+    """
+    Testing what happens if we lose a path for more than 30s
+    so that the program decides that we no longer can use the path.
+    Should result in re-gathering on this path because no connection
+    exists anymore.
+    """
+
+    h1 = net.get("h1")
+    h2 = net.get("h2")
+
+    target = conf.build_target
+    tp = conf.throughput
+    output_processes = []
+
+    if conf.log_level.value > Logging.INFO.value:
+        server = h2.popen(f"{quicheperf_dir}/target/{target}/quicheperf server --cert {quicheperf_dir}/src/cert.crt --key {quicheperf_dir}/src/cert.key -l 192.168.1.3:10000 --mp true &> {testing_dir}/{directory}/h2.log", shell=True)
+
+        client = h1.popen(f"{quicheperf_dir}/target/{target}/quicheperf client -l 192.168.1.2:20000 -c 192.168.1.3:10000 --mp true -d {conf.duration} -b {tp} &> {testing_dir}/{directory}/h1.log", shell=True)
+
+        server_capture = (server, None)
+        client_capture = (client, None)
+        output_processes.append(server_capture)
+        output_processes.append(client_capture)
+    else:
+        server = h2.popen(f"{quicheperf_dir}/target/{target}/quicheperf server --cert {quicheperf_dir}/src/cert.crt --key {quicheperf_dir}/src/cert.key -l 192.168.1.3:10000 --mp true", stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+        client = h1.popen(f"{quicheperf_dir}/target/{target}/quicheperf client -l 192.168.1.2:20000 -c 192.168.1.3:10000 --mp true -d {conf.duration} -b {tp}", stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    
+        server_capture = (server, f"{directory}/h2.log")
+        client_capture = (client, f"{directory}/h1.log")
+        output_processes.append(server_capture)
+        output_processes.append(client_capture)
+
+    # Waiting long enough so that at least one path has been found
+    wait(5)
+    path_loss(net, "h1", "h1-eth")
+    # Wait for all bindings to timeout
+    wait(35)
+    # Restore the path and allow for packets to flow
+    path_loss(net, "h1", "h1-eth", loss=0)
+    # Give enough time to restart and find the path
+    wait(15)
+
+    # Finished testing
     return output_processes
