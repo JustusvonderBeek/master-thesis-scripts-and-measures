a87166ed2cafee0690e7cbd82b18086a8f618482
Justus von der Beek <justusvonderbeek@gmail.com>
2024-06-18 10:14:53 +0200

    updated to single device

--- box_measurements/test_ios.sh
+++ box_measurements/test_ios.sh
@@ -7,29 +7,32 @@ test_kill() {
     ./pull_pcap.sh $insideBox $filename $localPcapFilePath
     ./pull_pcap.sh $outsideBox $filename $localPcapFilePath
 
-    echo "Android nearby share test ended"
+    echo "AirDrop test ended"
 }
 
 echo "Starting iOS device test..."
 
 # Get with: idevice_id -l
-iPhone="00008020-001E50A62684002E"
-iPad="00008027-000E11421187002E"
+# iPhone="00008020-001E50A62684002E"
+# iPad="00008027-000E11421187002E"
+iPad="00008027-001D28C60C87002E"
 
+dateNow="$(date +'%d_%m')"
+timeNow="$(date +'%H_%M')"
 if [[ $# -lt 2 ]]; then
-    localPcapFilePath="../airdropPcap/"
+    localPcapFilePath="measurements/airdropPcap/${dateNow}/${timeNow}"
 else
     localPcapFilePath="$2"
 fi
-timeNow="$(date +"%d_%m_%H_%M")"
-if [[ $# -lt 1 ]]; then
-    filename=$timeNow
-else
-    filename="$1_$timeNow"
-fi
-filename="$filename.pcap"
+
+mkdir -p "$localPcapFilePath"
+
+filename="ipad"
 
 # Hardcoded for this test
-(trap 'kill 0' SIGINT; $HOME/Documents/Code/rvi_capture/rvi_capture.py --udid $iPhone "${localPcapFilePath}iphone_$filename" & $HOME/Documents/Code/rvi_capture/rvi_capture.py --udid $iPad "${localPcapFilePath}ipad_$filename")
+# (trap 'kill 0' SIGINT; $HOME/Documents/Code/rvi_capture/rvi_capture.py --udid $iPhone "${localPcapFilePath}iphone_$filename" & $HOME/Documents/Code/rvi_capture/rvi_capture.py --udid $iPad "${localPcapFilePath}ipad_$filename")
+
+$HOME/Documents/Code/rvi_capture/rvi_capture.py --udid $iPad "${localPcapFilePath}/{filename}.pcap"
+
 
 echo "iOS device test finished"
