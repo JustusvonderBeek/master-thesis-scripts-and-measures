fa2cc30f358883d15355e1ba263a3559e0bd8056
Justus von der Beek <justusvonderbeek@gmail.com>
2024-06-22 23:58:48 +0200

    add test 2

--- plotting/plotPcap.py
+++ plotting/plotPcap.py
@@ -45,41 +45,41 @@ def plotThroughput(args):
     Gets the parsed command line and outputs the final throughput graph.
     """
 
-    interfaces=[
-        "h1-cellular", 
-        "h1-cellular", 
-        "h2-cellular", 
-        "h1-cellular"
-    ]
-    filterRules=[
-        "stun&&!icmp&&ip.addr==1.20.50.100", 
-        "stun&&!icmp&&ip.dst==1.20.50.20", 
-        "stun&&!icmp&&ip.src==1.20.50.10&&ip.dst==2.40.60.3", 
-        "!stun&&udp&&!mdns&&ip.dst==1.20.50.20"
-    ]
-    columns=[
-        "H1 STUN Address Resolution", 
-        "H1 -> H2 STUN Probes (out)",
-        "H1 -> H2 STUN Probes (in)", 
-        "H1 -> H2 Cellular (QUIC)"
-    ]
-    resolution="0,005"
     # interfaces=[
-    #     "h1-wifi", 
-    #     "h1-eth", 
     #     "h1-cellular", 
+    #     "h1-cellular", 
+    #     "h2-cellular", 
+    #     "h1-cellular"
     # ]
     # filterRules=[
-    #     "!stun&&!mdns&&udp", 
-    #     "!stun&&!mdns&&udp", 
-    #     "!stun&&!mdns&&udp", 
+    #     "stun&&!icmp&&ip.addr==1.20.50.100", 
+    #     "stun&&!icmp&&ip.dst==1.20.50.20", 
+    #     "stun&&!icmp&&ip.src==1.20.50.10&&ip.dst==2.40.60.3", 
+    #     "!stun&&udp&&!mdns&&ip.dst==1.20.50.20"
     # ]
     # columns=[
-    #     "H1 Wi-Fi", 
-    #     "H1 Ethernet",
-    #     "H1 Cellular", 
+    #     "H1 STUN Address Resolution", 
+    #     "H1 -> H2 STUN Probes (out)",
+    #     "H1 -> H2 STUN Probes (in)", 
+    #     "H1 -> H2 Cellular (QUIC)"
     # ]
-    # resolution="0,05"
+    # resolution="0,005"
+    interfaces=[
+        "h1-wifi", 
+        "h1-eth", 
+        "h1-cellular", 
+    ]
+    filterRules=[
+        "!stun&&!mdns&&udp", 
+        "!stun&&!mdns&&udp", 
+        "!stun&&!mdns&&udp", 
+    ]
+    columns=[
+        "H1 Wi-Fi", 
+        "H1 Ethernet",
+        "H1 Cellular", 
+    ]
+    resolution="1"
     data = parsePcap(args.input[0], resolution, interfaces, filterRules, columns)
 
     # --------------------
@@ -97,8 +97,8 @@ def plotThroughput(args):
 
 
     xmax = data["Interval"][len(data["Interval"])-1]
-    axes.set_xlim(xmin=4.28, xmax=5.65)
-    # axes.set_xlim(xmin=0, xmax=xmax)
+    # axes.set_xlim(xmin=4.28, xmax=5.65)
+    axes.set_xlim(xmin=0, xmax=xmax)
     
     # Scale the ticks in case we have too many (aka more than 20)
     # print(len(data["Interval"]))
@@ -114,15 +114,6 @@ def plotThroughput(args):
     #     axes.xaxis.set_major_locator(loc)
     #     plt.xticks(rotation=0)
 
-    loc = plticker.MultipleLocator(base=0.1)
-    minor_loc = plticker.MultipleLocator(base=0.02)
-    axes.xaxis.set_major_locator(loc)
-    axes.xaxis.set_minor_locator(minor_loc)
-    # axes.yaxis.set_minor_locator(minor_loc)
-
-    plt.grid(True, axis="x")
-    axes.grid(which="minor", linestyle="dotted", linewidth='0.5', color="gray")
-    # plt.xticks(rotation=90)
 
     # print(max_index)
     # axes.xaxis.set_ticks(np.arange(0, max_index, 5))
@@ -137,16 +128,28 @@ def plotThroughput(args):
         axes.set_yscale('log',base=10)
         axes.set_ylim(ymin=10e-1)
     else:
-        # axes.set_ylim(ymin=0)
-        axes.set_ylim(ymin=0, ymax=4)
+        axes.set_ylim(ymin=0)
+        # axes.set_ylim(ymin=0, ymax=4)
     
     axes.yaxis.set_major_formatter(ScalarFormatter())
     axes.set(xlabel="Time in seconds")
     axes.set(ylabel="Packets per second")
 
+    loc = plticker.MultipleLocator(base=5)
+    # loc = plticker.MultipleLocator(base=0.1)
+    # minor_loc = plticker.MultipleLocator(base=0.02)
+    minor_loc = plticker.MultipleLocator(base=1)
+    axes.xaxis.set_major_locator(loc)
+    axes.xaxis.set_minor_locator(minor_loc)
+    # axes.yaxis.set_minor_locator(minor_loc)
+
+    axes.grid(which="minor", linestyle="dotted", linewidth='0.5', color="gray")
+    plt.grid(True)
+    # plt.xticks(rotation=90)
+
     # axes.legend(loc="upper right", title="Interface", fancybox=True, framealpha=0.9)
-    axes.legend(loc="best", title="Packet Types", fancybox=True, framealpha=0.9)
-    plt.title("Cellular Path Building in Detail")
+    axes.legend(loc="best", title="Interfaces", fancybox=True, framealpha=0.9)
+    plt.title("Prototype Path Migration and Rebuilding")
 
     # # Adding some figure custom annotations
     # plt.axvline(x=1.29, color=(1, 0, 0, 1), linestyle="--")
@@ -161,18 +164,39 @@ def plotThroughput(args):
     
     
     # Adding the cellular path finding annotation
-    plt.axvline(x=4.37, color=(1, 0, 0, 1), linestyle="--")
-    plt.axvline(x=4.87, color=(1, 0, 0, 1), linestyle="--")
+    # plt.axvline(x=4.37, color=(1, 0, 0, 1), linestyle="--")
+    # plt.axvline(x=4.87, color=(1, 0, 0, 1), linestyle="--")
     
-    plt.annotate("1. Finished resolving peer-\nreflexive address via TURN\nserver", xy=(4.37, 3.12), xytext=(4.41, 3.45), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    # plt.annotate("1. Finished resolving peer-\nreflexive address via TURN\nserver", xy=(4.37, 3.12), xytext=(4.41, 3.45), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
     
-    plt.annotate("2. Both peers\nstart probing,\nrepeating every\n~200ms", xy=(4.45, 2), xytext=(4.5, 2.5), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    # plt.annotate("2. Both peers\nstart probing,\nrepeating every\n~200ms", xy=(4.45, 2), xytext=(4.5, 2.5), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
 
-    plt.annotate("3. First probe\narrives at H2\n~36ms after\nsending (only\nreflexive probe\nreaches H2)", xy=(4.49, 1), xytext=(4.57, 0.5), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    # plt.annotate("3. First probe\narrives at H2\n~36ms after\nsending (only\nreflexive probe\nreaches H2)", xy=(4.49, 1), xytext=(4.57, 0.5), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
 
-    plt.annotate("4. Full STUN Handshake\ncompleted", xy=(4.86, 3), xytext=(5.12, 2.5), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    # plt.annotate("4. Full STUN Handshake\ncompleted", xy=(4.86, 3), xytext=(5.12, 2.5), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
 
-    plt.annotate("5. Path added to\nQUIC and used\n~690ms after\nHandshake\ncomplete", xy=(5.56, 1), xytext=(5.33, 1.31), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    # plt.annotate("5. Path added to\nQUIC and used\n~690ms after\nHandshake\ncomplete", xy=(5.56, 1), xytext=(5.33, 1.31), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+
+    # Annotations for migration
+    plt.axvline(x=7, color=(1, 0, 0, 1), linestyle="--")
+    plt.axvline(x=11, color=(1, 0, 0, 1), linestyle="--")
+    
+    plt.axvline(x=31, color=(1, 0, 0, 1), linestyle="--")
+    plt.axvline(x=44, color=(1, 0, 0, 1), linestyle="--")
+    
+    plt.annotate("1. Ethernet path\n100% loss and\nmigration to Wi-Fi", xy=(7, 80), xytext=(13.5, 29), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    
+    plt.annotate("2. Wi-Fi path\n100% loss and\nmigration to\nCellular", xy=(11, 20), xytext=(15.5, 9), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    
+    plt.annotate("3. STUN probes\nfail", xy=(15, 3), xytext=(15.5, 4), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    
+    plt.annotate("4. Ethernet path\nre-enabled", xy=(31, 45), xytext=(47, 42), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    
+    plt.annotate("5. Migration back\nto Ethernet path", xy=(32, 100), xytext=(47, 80), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    
+    plt.annotate("6. Wi-Fi path\nre-enabled", xy=(44, 7), xytext=(49, 6), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+
+    plt.annotate("7. It takes until\nthe next iteration\nto consider the\nWi-Fi path for\nprobing", xy=(63, 3), xytext=(46, 1.3), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
 
     exportToPdf(fig, args.output)
 
