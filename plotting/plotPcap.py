84885d2f65999ef2764d1734db9130046c4bc36c
Justus von der Beek <justusvonderbeek@gmail.com>
2024-07-11 23:27:55 +0200

    updated to final plots

--- plotting/plotPcap.py
+++ plotting/plotPcap.py
@@ -45,52 +45,52 @@ def plotThroughput(args):
     Gets the parsed command line and outputs the final throughput graph.
     """
 
-    interfaces=[
-        "h1-cellular", 
-        "h1-cellular", 
-        "h2-cellular", 
-        "h1-cellular"
-    ]
-    filterRules=[
-        "stun&&!icmp&&ip.addr==1.20.50.100", 
-        "stun&&!icmp&&ip.dst==1.20.50.20", 
-        "stun&&!icmp&&ip.src==1.20.50.10&&ip.dst==2.40.60.3", 
-        "!stun&&udp&&!mdns&&ip.dst==1.20.50.20"
-    ]
-    columns=[
-        "H1 STUN Address Resolution", 
-        "H1 -> H2 STUN Probes (out)",
-        "H1 -> H2 STUN Probes (in)", 
-        "H1 -> H2 Cellular (QUIC)"
-    ]
-    resolution="0,005"
     # interfaces=[
-    #     # "h1-wifi", 
-    #     "h1-eth", 
-    #     "h1-cellular",
-    #     "h1-eth", 
-    #     # "h1-cellular", 
-    #     # "h2-eth",
-    #     # "h2-cellular"
+    #     "h1-cellular", 
+    #     "h1-cellular", 
+    #     "h2-cellular", 
+    #     "h1-cellular"
     # ]
     # filterRules=[
-    #     # "!stun&&!mdns&&udp&&ip.dst==192.168.1.3", 
-    #     "!stun&&!mdns&&udp&&(quic.path_challenge.data||quic.path_response.data)", 
-    #     "!mdns&&udp&&!icmp&&!stun&&(ip.dst==1.20.50.100||ip.dst==1.20.50.20)", 
-    #     "!stun&&!mdns&&udp&&!(quic.path_challenge.data||quic.path_response.data)", 
-    #     # "udp&&!icmp&&!stun&&ip.dst==1.20.50.20&&!(quic.path_challenge.data||quic.path_response.data)",
-    #     # "!stun&&!mdns&&udp&&ip.dst==172.16.2.20",
-    #     # "!stun&&!mdns&&udp&&ip.dst==2.40.60.3",
+    #     "stun&&!icmp&&ip.addr==1.20.50.100", 
+    #     "stun&&!icmp&&ip.dst==1.20.50.20", 
+    #     "stun&&!icmp&&ip.src==1.20.50.10&&ip.dst==2.40.60.3", 
+    #     "!stun&&udp&&!mdns&&ip.dst==1.20.50.20"
     # ]
     # columns=[
-    #     # "H1 Wi-Fi", 
-    #     "H1 Ethernet (QUIC Probes)",
-    #     "H1 Cellular (QUIC Probes)",
-    #     "H1 Ethernet (Data)",
-    #     # "H1 Cellular (Data)",
-    #     # "H2 Cellular (in)"
+    #     "H1 STUN Address Resolution", 
+    #     "H1 -> H2 STUN Probes (out)",
+    #     "H1 -> H2 STUN Probes (in)", 
+    #     "H1 -> H2 Cellular (QUIC)"
     # ]
     # resolution="0,005"
+    interfaces=[
+        # "h1-wifi", 
+        "h1-eth", 
+        "h1-cellular",
+        "h1-eth", 
+        # "h1-cellular", 
+        # "h2-eth",
+        # "h2-cellular"
+    ]
+    filterRules=[
+        # "!stun&&!mdns&&udp&&ip.dst==192.168.1.3", 
+        "!stun&&!mdns&&udp&&(quic.path_challenge.data||quic.path_response.data)", 
+        "!mdns&&udp&&!icmp&&!stun&&(ip.dst==1.20.50.100||ip.dst==1.20.50.20)", 
+        "!stun&&!mdns&&udp&&!(quic.path_challenge.data||quic.path_response.data)", 
+        # "udp&&!icmp&&!stun&&ip.dst==1.20.50.20&&!(quic.path_challenge.data||quic.path_response.data)",
+        # "!stun&&!mdns&&udp&&ip.dst==172.16.2.20",
+        # "!stun&&!mdns&&udp&&ip.dst==2.40.60.3",
+    ]
+    columns=[
+        # "H1 Wi-Fi", 
+        "H1 Ethernet (QUIC Probes)",
+        "H1 Cellular (QUIC Probes)",
+        "H1 Ethernet (Data)",
+        # "H1 Cellular (Data)",
+        # "H2 Cellular (in)"
+    ]
+    resolution="0,005"
     data = parsePcap(args.input[0], resolution, interfaces, filterRules, columns)
 
     # --------------------
@@ -129,8 +129,8 @@ def plotThroughput(args):
 
     xmax = data["Interval"][len(data["Interval"])-1]
     # print(xmax)
-    axes.set_xlim(xmin=4.153, xmax=5.65)
-    # axes.set_xlim(xmin=0.65, xmax=1.2)
+    # axes.set_xlim(xmin=4.153, xmax=5.65)
+    axes.set_xlim(xmin=0.7, xmax=1.3)
     # axes.set_xlim(xmin=0, xmax=xmax)
     
     # Scale the ticks in case we have too many (aka more than 20)
@@ -171,7 +171,8 @@ def plotThroughput(args):
     loc = plticker.MultipleLocator(base=0.1)
     # loc = plticker.MultipleLocator(base=5)
     # loc = plticker.MultipleLocator(base=0.1)
-    minor_loc = plticker.MultipleLocator(base=0.02)
+    # minor_loc = plticker.MultipleLocator(base=0.02)
+    minor_loc = plticker.MultipleLocator(base=0.01)
     # minor_loc = plticker.MultipleLocator(base=1)
     axes.xaxis.set_major_locator(loc)
     axes.xaxis.set_minor_locator(minor_loc)
@@ -181,11 +182,11 @@ def plotThroughput(args):
     plt.grid(True, axis="x")
     # plt.xticks(rotation=90)
 
-    axes.legend(loc="upper right", title="Interface", fancybox=True, framealpha=0.9)
+    # axes.legend(loc="upper right", title="Interface", fancybox=True, framealpha=0.9)
     # axes.legend(loc="best", title="Interfaces", fancybox=True, framealpha=0.9)
-    # axes.legend(loc=(0.6,0.78), title="Interfaces", fancybox=True, framealpha=0.9)
-    plt.title("Cellular Path Building in Detail")
-    # plt.title("QUIC Path Probing")
+    axes.legend(loc=(0.6,0.78), title="Interfaces", fancybox=True, framealpha=0.9)
+    # plt.title("Cellular Path Building in Detail")
+    plt.title("QUIC Path Probing")
 
 
     # # Adding some figure custom annotations for Pathfinding Test 1
@@ -206,22 +207,22 @@ def plotThroughput(args):
     
     
     # Adding the cellular path finding annotation Test 1
-    plt.axvline(x=4.37, color=(1, 0, 0, 1), linestyle="--")
-    plt.axvline(x=4.87, color=(1, 0, 0, 1), linestyle="--")
-    plt.axvline(x=5.27, color=(1, 0, 0, 1), linestyle="--")
+    # plt.axvline(x=4.37, color=(1, 0, 0, 1), linestyle="--")
+    # plt.axvline(x=4.87, color=(1, 0, 0, 1), linestyle="--")
+    # plt.axvline(x=5.27, color=(1, 0, 0, 1), linestyle="--")
     
-    plt.annotate("1. Finished resolving peer-\nreflexive address via TURN\nserver", xy=(4.37, 3.12), xytext=(4.41, 3.45), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5", alpha=0.8))
+    # plt.annotate("1. Finished resolving peer-\nreflexive address via TURN\nserver", xy=(4.37, 3.12), xytext=(4.41, 3.45), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5", alpha=0.8))
     
-    plt.annotate("2. Both peers\nstart probing,\nrepeating every\n~200ms", xy=(4.45, 2), xytext=(4.5, 2.5), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    # plt.annotate("2. Both peers\nstart probing,\nrepeating every\n~200ms", xy=(4.45, 2), xytext=(4.5, 2.5), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
 
-    plt.annotate("3. First probe\narrives at H2\n~36ms after\nsending (only\nreflexive probe\nreaches H2)", xy=(4.49, 1), xytext=(4.57, 0.5), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5", alpha=0.8))
+    # plt.annotate("3. First probe\narrives at H2\n~36ms after\nsending (only\nreflexive probe\nreaches H2)", xy=(4.49, 1), xytext=(4.57, 0.5), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5", alpha=0.8))
 
-    plt.annotate("4. Full STUN\nHandshake\ncompleted,\nfirst\nUSE_CANDIDATE\nsent", xy=(4.86, 3), xytext=(4.925, 2.13), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.4", alpha=0.8))
+    # plt.annotate("4. Full STUN\nHandshake\ncompleted,\nfirst\nUSE_CANDIDATE\nsent", xy=(4.86, 3), xytext=(4.925, 2.13), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.4", alpha=0.8))
 
-    plt.annotate("5. Candidate\nnominated", xy=(5.27, 2), xytext=(5.32, 2.5), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    # plt.annotate("5. Candidate\nnominated", xy=(5.27, 2), xytext=(5.32, 2.5), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
 
-    # Difference: 5,557687136−5,271353439 = 0,286333697
-    plt.annotate("6. Path added to\nQUIC and used\n~286ms after\nNomination", xy=(5.56, 1), xytext=(5.33, 1.31), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    # # Difference: 5,557687136−5,271353439 = 0,286333697
+    # plt.annotate("6. Path added to\nQUIC and used\n~286ms after\nNomination", xy=(5.56, 1), xytext=(5.33, 1.31), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
 
 
     # # Annotations for migration Test 2
@@ -255,14 +256,14 @@ def plotThroughput(args):
     # plt.axvspan(73, 75, facecolor="gray", alpha=0.3)
 
     # Annotations for QUIC Probing Test 3
-    # plt.axvline(x=0.68, color=(1, 0, 0, 1), linestyle="--")
-    # plt.axvline(x=1.17, color=(1, 0, 0, 1), linestyle="--")
+    plt.axvline(x=0.71, color=(1, 0, 0, 1), linestyle="--")
+    plt.axvline(x=1.243, color=(1, 0, 0, 1), linestyle="--")
 
-    # plt.axvspan(0.68, 0.78, facecolor="gray", alpha=0.3)
+    plt.axvspan(0.71, 0.81, facecolor="gray", alpha=0.3)
 
-    # plt.annotate("1. QUIC Path\nChallenge and\nResponses\n(ingress,egress)", xy=(0.74, 1), xytext=(0.7, 2.2), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    plt.annotate("1. QUIC Path\nChallenge and\nResponses\n(ingress,egress)", xy=(0.791, 1), xytext=(0.73, 2.2), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
 
-    # plt.annotate("2. QUIC Probing\non Cellular every\n~1s", xy=(0.98, 1), xytext=(0.84, 3.2), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
+    plt.annotate("2. QUIC Probing\non Cellular every\n~1s", xy=(1.02, 1), xytext=(0.9, 3.2), arrowprops=dict(arrowstyle="->", color="black"), bbox=dict(facecolor="white", boxstyle="round,pad=0.5"))
 
     exportToPdf(fig, args.output)
 
